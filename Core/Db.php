2c7a4c017efda60b7874496974fb8528d36e8717
emmanuel cabassot <emmanuel.cabassot@laplateforme.io>
2022-09-12 23:23:11 +0200

    change Core

--- Core/Db.php
+++ Core/Db.php
@@ -14,30 +14,30 @@ class Db extends PDO
     // Informations de connexion
     private const DBHOST = 'localhost:3306';
     private const DBUSER = 'emmanuel';
-    private const DBPASS = '*****';
+    private const DBPASS = '******';
     private const DBNAME = 'emmanuel-cabassot_boutique';
 
     private function __construct()
     {
         // DSN de connexion
-         $_dsn = 'mysql:dbname='. self::DBNAME . ';charset=utf8; host=' . self::DBHOST;
+        $_dsn = 'mysql:dbname=' . self::DBNAME . ';charset=utf8; host=' . self::DBHOST;
 
         // On appelle le constructeur de la classe PDO
-        try{
+        try {
             parent::__construct($_dsn, self::DBUSER, self::DBPASS);
 
             $this->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, 'SET NAMES utf8');
             $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
             $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }catch(PDOException $e){
+        } catch (PDOException $e) {
             die($e->getMessage());
         }
     }
 
 
-    public static function getInstance():self
+    public static function getInstance(): self
     {
-        if(self::$instance === null){
+        if (self::$instance === null) {
             self::$instance = new self();
         }
         return self::$instance;
